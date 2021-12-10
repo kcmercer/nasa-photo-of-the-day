bbc916e641773b394ef02263a2421754e879efd0
Kenneth Mercer <kmercer5439@gmail.com>
2021-12-09 20:59:41 -0600

    converted most css styling to js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import styled from 'styled-components'
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
 import "./App.css";
+
+const StyledApp = styled.div`
+  background-color: #264653;
+`
+const StyledLore = styled.div`
+  font-family: 'Times New Roman';
+  word-spacing: 1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+`
+
+const StyledH1 = styled.h1`
+  padding-top: 1%;
+  font-family: 'Times New Roman';
+  color: #2A9D8F;
+`
+
+const StyledH2 = styled.h2`
+width: 100%;
+font-size: 3rem;
+color: #2A9D8F;
+margin-top: 2;
+margin-bottom: 0;
+`
+
+const StyledP = styled.p`
+  font-size: 1.25rem;
+  width: 50%;
+  color: #E9C46A;
+  padding: 1%;
+  border: 2px black solid;
+  border-radius: 5px;
+`
 
 function App() {
   const [nasaData, setNasaData] = useState([]);
@@ -12,19 +48,19 @@ function App() {
         })
       }, [])
   return (
-    <div className="App">
-      <h1>Nasa's Photo of the Day!</h1>
+    <StyledApp className="App">
+      <StyledH1>Nasa's Photo of the Day!</StyledH1>
       <img src={nasaData.hdurl} alt={nasaData.explanation}></img>
-      <div className="photoLore">
-        <h2>{nasaData.title}</h2>
-        <p>{nasaData.explanation}</p>
-      </div>
+      <StyledLore>
+        <StyledH2>{nasaData.title}</StyledH2>
+        <StyledP>{nasaData.explanation}</StyledP>
+      </StyledLore>
       <div className="photoInfo">
         <h4>Additional Information</h4>
         <p>Copyright - {nasaData.copyright}</p>
         <p>Date Taken - {nasaData.date}</p>
       </div>
-    </div>
+    </StyledApp>
   );
 }
 
